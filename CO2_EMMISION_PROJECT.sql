b761edcd8b7da1745fb873c89d913d89fd6f0f78
Surckerg <67701907+Surckerg@users.noreply.github.com>
2023-04-02 22:06:57 +0100

    Update CO2_EMMISION_PROJECT.sql

--- CO2_EMMISION_PROJECT.sql
+++ CO2_EMMISION_PROJECT.sql
@@ -1,4 +1,4 @@
-##Check my dataset
+--Check my dataset
 
 SELECT *
 FROM VEHICLES_C02_EMMISIONS vce 
@@ -7,24 +7,24 @@ LIMIT 10;
 SELECT MEDIAN("Engine Size(L)") AS Median_Engine_Size
 from VEHICLES_C02_EMMISIONS vce 
 
-##Average fuel consumption per fuel type
+--Average fuel consumption per fuel type
 
 SELECT "Fuel Type", AVG("Fuel Consumption Comb (L/100 km)") as average_fuel_consumption
 FROM VEHICLES_C02_EMMISIONS vce 
 GROUP BY "Fuel Type" 
 
-##We need to get the correlation between co2 emmision and fuel consumption
+--We need to get the correlation between co2 emmision and fuel consumption
 
 
 SELECT "Fuel Consumption Comb (L/100 km)" as fuel_consumption,"CO2 Emissions(g/km)" as co2_emmison
 from VEHICLES_C02_EMMISIONS vce
 LIMIT 10;
 
-##We export the data for further analysis in R
+--We export the data for further analysis in R
 
 
 
-```vehicle types with lower average CO2 emissions(SUV -SMALL OR MID-SIZE)```
+--vehicle types with lower average CO2 emissions(SUV -SMALL OR MID-SIZE)```
 
 SELECT AVG("CO2 Emissions(g/km)") AS Avg_CO_emissions,"Vehicle Class" 
 FROM VEHICLES_C02_EMMISIONS vce 
